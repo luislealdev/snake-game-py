258c1e508be96d1f835cca771108e95cc44bbc11
Luis Leal <luisrrleal@gmail.com>
2023-03-29 12:45:05 -0600

    Add read and write file

--- scoreboard.py
+++ scoreboard.py
@@ -8,17 +8,27 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.hight_score = 0
+        self.hight_score = self.get_hight_score()
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.update_scoreboard()
         self.hideturtle()
 
+    def get_hight_score(self):
+        with open("high_score.txt") as file:
+            score = int(file.read())
+        return score
+    
     def update_scoreboard(self):
         if self.score > self.hight_score:
             self.hight_score = self.score
+            self.update_high_score()
         self.write(f"Score: {self.score} Best Score: {self.hight_score}", align=ALIGMENT, font=FONT)
+    
+    def update_high_score(self):
+        with open("high_score.txt", "w") as file:
+            file.write(str(self.score))
     
     def reset(self):
         self.clear()
